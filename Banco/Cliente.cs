fc1310f50e72af1f316e0e9bdd9ee750c0a0787b
ManuelMontes9 <32351849+ManuelMontes9@users.noreply.github.com>
2017-11-08 20:52:03 -0600

    Banco

---
+++ Banco/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class Cliente
+    {
+        public string nombre { get; set; }
+        public float saldo { get; set; }
+        public byte edad { get; set; }
+
+
+
+    }
+}
